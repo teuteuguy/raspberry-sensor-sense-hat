857910be6ec95f1a93d90e27370dee39691efb65
teuteuguy <pub@lalatte.com>
2015-11-06 15:35:14 +0800

    Updated script for tests

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get -y install sense-hat
-sudo pip-3.2 install pillow
+#sudo apt-get update
+#sudo apt-get -y install sense-hat
+#sudo pip-3.2 install pillow
